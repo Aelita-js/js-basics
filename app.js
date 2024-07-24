a8b11f88b8731850a267b3a8801c164658e60810
Aelita Imanalieva <aiko.imanalieva00@gmail.com>
2024-07-24 18:37:36 +0600

    feat: functions. 3rd task

--- app.js
+++ app.js
@@ -2,60 +2,121 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+let numberOfFilms;
 
-console.log(numberOfFilms);
 
+
+function start () {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+    }
+}
+
+start();
+
+
+
+
+// ===== Global DB Object ============
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: true
 };
 
 
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Последний посмотренный фильм?");
-    let b = prompt('На сколько его оцените?');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Done!');
-    } else {
-        console.log('Error!');
-        i--;
+function rememberMyFilms () {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Последний посмотренный фильм?");
+        let b = prompt('На сколько его оцените?');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('Done!');
+        } else {
+            console.log('Error!');
+            i--;
+        }
     }
 }
 
+// rememberMyFilms();
 
-if (personalMovieDB.count < 10) {
-    alert("Просмотрено довольно мало фильмов");
-    console.log("Просмотрено довольно мало фильмов");
-} else if (10 >= personalMovieDB.count && personalMovieDB.count <= 30) {
-    alert("Вы классический зритель");
-    console.log("Вы классический зритель");
-} else if (30 <= personalMovieDB.count) {
-    alert("Вы киноман");
-    console.log("Вы киноман");
-} else {
-    alert("Произошла ошибка");
-    console.log("Произошла ошибка");
+
+
+
+function detectPersonalLevel () {
+    if (personalMovieDB.count < 10) {
+        alert("Просмотрено довольно мало фильмов");
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (10 >= personalMovieDB.count && personalMovieDB.count <= 30) {
+        alert("Вы классический зритель");
+        console.log("Вы классический зритель");
+    } else if (30 <= personalMovieDB.count) {
+        alert("Вы киноман");
+        console.log("Вы киноман");
+    } else {
+        alert("Произошла ошибка");
+        console.log("Произошла ошибка");
+    }
 }
 
-console.log(personalMovieDB);
+// detectPersonalLevel();
 
-// ========= Задание 2 =========================================
 
-/* 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. 
-Если это происходит - возвращаем пользователя к вопросам опять. 
-(К любой строке можно обратиться как 
-str.length - и получить её длину)
 
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка" */
+// function showMyDB (property) {
+//     if (property.privat) {
+//         console.log(property);
+//     }
+// }
+// showMyDB(personalMovieDB);
+
+function showMyDB (property) {
+    if (!property) {
+        console.log(personalMovieDB);
+    }
+}
+
+// showMyDB(personalMovieDB.privat)
+
+
+
+
+function writeYourGenres () {
+    for (let i = 1; i <= 3; i++) {
+        let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+        if (genre == '' || genre == null) {
+            i--;
+        } else {
+            personalMovieDB.genres[i-1] = genre;
+        }
+
+    }
+    console.log(personalMovieDB);
+}
+
+writeYourGenres();
+
+
+
+// ========= Задание 3 =========================================
+
+/* 
+1) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+2) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно 
+*/
